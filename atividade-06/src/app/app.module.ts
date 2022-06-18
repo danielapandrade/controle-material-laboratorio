88a6cbe82ff45e9127b51d2199540d8c6e0ac78e
danielapandrade <danielandraderock@gmail.com>
2022-06-17 23:29:58 -0300

    deletando componentes body e login

--- atividade-06/src/app/app.module.ts
+++ atividade-06/src/app/app.module.ts
@@ -5,14 +5,13 @@ import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
-import { BodyComponent } from './body/body.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
-    BodyComponent,
+
   ],
   imports: [
     BrowserModule,
